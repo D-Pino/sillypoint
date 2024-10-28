7ae6a0f28269bf969442c21e407729b9c7566af4
Daniel Pino <danielpino1121@gmail.com>
2024-10-28 19:37:52 -0400

    Add depth model submodule and re-organize some of the image proc stuff (#3)
    Add depth model submodule and do some re-org

---
+++ firstslip/test_streamlit.py
@@ -0,0 +1,31 @@
+import pydeck
+import pandas as pd
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+DATA_URL = "https://raw.githubusercontent.com/ajduberstein/geo_datasets/master/small_waterfall.csv"
+df = pd.read_csv(DATA_URL)
+
+target = [df.x.mean(), df.y.mean(), df.z.mean()]
+
+point_cloud_layer = pydeck.Layer(
+    "PointCloudLayer",
+    data=DATA_URL,
+    get_position=["x", "y", "z"],
+    get_color=["r", "g", "b"],
+    get_normal=[0, 0, 15],
+    auto_highlight=True,
+    pickable=True,
+    point_size=3,
+)
+
+view_state = pydeck.ViewState(target=target, controller=True, rotation_x=15, rotation_orbit=30, zoom=5.3)
+view = pydeck.View(type="OrbitView", controller=True)
+
+deck = pydeck.Deck(point_cloud_layer, initial_view_state=view_state, views=[view])
+deck_html_str = deck.to_html(as_string=True, css_background_color="#add8e6")
+
+if __name__ == "__main__":
+    components.html(deck_html_str, height=800)
+    st.pydeck_chart(deck)
