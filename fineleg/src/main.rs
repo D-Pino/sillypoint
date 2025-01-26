31a72cd58607c495d02a936aa68a9d03b0e970cd
Daniel Pino <danielpino1121@gmail.com>
2025-01-26 00:02:39 -0400

    Add rerun and touchup README (#7)
    * Add rerun
    
    * EOF newline

--- fineleg/src/main.rs
+++ fineleg/src/main.rs
@@ -1,25 +1,19 @@
-use anyhow::{anyhow, Result};
-use fineleg::scratchpad::{get_character_data_async, get_image};
-use polars::frame::DataFrame;
-use tokio::runtime::Runtime;
+use anyhow::Result;
+use rerun::{demo_util::grid, external::glam};
 
 fn main() -> Result<()> {
-    let characters_data: DataFrame = Runtime::new()?.block_on(get_character_data_async(10))?;
-    println!("{characters_data}");
-    for character_url in characters_data.column("url")?.str()? {
-        if let Some(url) = character_url {
-            println!("Getting image from {url}...");
-            let image = get_image(&url)?;
-            let output_path_base = "../common/images/";
-            let output_path = format!(
-                "{}{}.jpg",
-                output_path_base,
-                url.split("/")
-                    .last()
-                    .ok_or_else(|| anyhow!("Failed to get character name"))?
-            );
-            image.save_with_format(output_path, image::ImageFormat::Jpeg)?;
-        }
-    }
+    let rec = rerun::RecordingStreamBuilder::new("rerun_example_minimal").spawn()?;
+
+    let points = grid(glam::Vec3::splat(-10.0), glam::Vec3::splat(10.0), 10);
+    let colors = grid(glam::Vec3::ZERO, glam::Vec3::splat(255.0), 10)
+        .map(|v| rerun::Color::from_rgb(v.x as u8, v.y as u8, v.z as u8));
+
+    rec.log(
+        "my_points",
+        &rerun::Points3D::new(points)
+            .with_colors(colors)
+            .with_radii([0.5]),
+    )?;
+
     Ok(())
 }
--- fineleg/src/scratchpad.rs
+++ fineleg/src/scratchpad.rs
@@ -81,3 +81,29 @@ pub fn get_image(character_url: &str) -> Result<image::DynamicImage> {
     let image_bytes = reqwest::blocking::get(image_url)?.bytes()?;
     Ok(image::load_from_memory(&image_bytes)?)
 }
+
+// use fineleg::scratchpad::{get_character_data_async, get_image};
+// use polars::frame::DataFrame;
+// use tokio::runtime::Runtime;
+
+
+// fn main() -> Result<()> {
+//     let characters_data: DataFrame = Runtime::new()?.block_on(get_character_data_async(10))?;
+//     println!("{characters_data}");
+//     for character_url in characters_data.column("url")?.str()? {
+//         if let Some(url) = character_url {
+//             println!("Getting image from {url}...");
+//             let image = get_image(&url)?;
+//             let output_path_base = "../common/images/";
+//             let output_path = format!(
+//                 "{}{}.jpg",
+//                 output_path_base,
+//                 url.split("/")
+//                     .last()
+//                     .ok_or_else(|| anyhow!("Failed to get character name"))?
+//             );
+//             image.save_with_format(output_path, image::ImageFormat::Jpeg)?;
+//         }
+//     }
+//     Ok(())
+// }
