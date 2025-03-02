87b1eb20bf0b020268adc1b99bb008542abda639
Daniel Pino <danielpino1121@gmail.com>
2025-03-02 15:58:27 -0400

    Fix rerun, and some code hygiene changes

--- fineleg/src/main.rs
+++ fineleg/src/main.rs
@@ -5,15 +5,18 @@ use fineleg::viz::{render_pointcloud_in_plotly, render_pointcloud_in_rerun};
 fn main() -> Result<()> {
     // This pointcloud has about 21M points, yikes
     let pointcloud = load_las_to_df("../common/data/pointclouds/2555_1137.las")?;
+    println!("Full pointcloud: {:?}", pointcloud);
 
     // Let's downsample so my machine survives rendering
-    let pointcloud_downsampled_r = pointcloud.sample_n_literal(350_000, false, false, None)?;
-    // let pointcloud_downsampled_v = voxel_downsample(pointcloud, 100_000)?;
-    let centered = center_pointcloud(pointcloud_downsampled_r)?;
-    render_pointcloud_in_plotly(centered)?;
+    // let pointcloud_downsampled = pointcloud.sample_n_literal(350_000, false, false, None)?;
+    let pointcloud_downsampled = voxel_downsample(pointcloud, 1_000_000)?;
+    println!("Downsampled pointcloud: {:?}", pointcloud_downsampled);
 
-    // let rec = rerun::RecordingStreamBuilder::new("switzerland").spawn()?;
-    // render_pointcloud_in_rerun(rec, "points", pointcloud_downsampled, None)?;
+    // let centered = center_pointcloud(pointcloud_downsampled)?;
+    // render_pointcloud_in_plotly(pointcloud_downsampled.clone())?;
+
+    let rec = rerun::RecordingStreamBuilder::new("switzerland").spawn()?;
+    render_pointcloud_in_rerun(rec, "points", pointcloud_downsampled, None)?;
 
     Ok(())
 }
