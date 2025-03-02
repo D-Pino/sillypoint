27c0c48c640b9dd2909271f4e6d14f01d8689033
Daniel Pino <danielpino1121@gmail.com>
2025-03-02 00:48:52 -0400

    Some updates to pointcloud utilities (#18)
    * Trying voxel downsampling
    
    * Add more stuff

--- fineleg/src/lib.rs
+++ fineleg/src/lib.rs
@@ -1,3 +1,5 @@
-pub mod scratchpad;
-pub mod las_utils;
+pub mod pointcloud_utils;
+pub mod viz;
+// These are really just me messing around, I don't have plans for these
 pub mod apod;
+pub mod scratchpad;
--- fineleg/src/main.rs
+++ fineleg/src/main.rs
@@ -1,13 +1,19 @@
 use anyhow::Result;
-use fineleg::las_utils::{load_las_to_df, render_las_in_rerun};
+use fineleg::pointcloud_utils::{center_pointcloud, load_las_to_df, voxel_downsample};
+use fineleg::viz::{render_pointcloud_in_plotly, render_pointcloud_in_rerun};
 
 fn main() -> Result<()> {
+    // This pointcloud has about 21M points, yikes
     let pointcloud = load_las_to_df("../common/data/pointclouds/2555_1137.las")?;
-    println!("{:?}", pointcloud);
 
+    // Let's downsample so my machine survives rendering
+    let pointcloud_downsampled_r = pointcloud.sample_n_literal(350_000, false, false, None)?;
+    // let pointcloud_downsampled_v = voxel_downsample(pointcloud, 100_000)?;
+    let centered = center_pointcloud(pointcloud_downsampled_r)?;
+    render_pointcloud_in_plotly(centered)?;
 
-    let rec = rerun::RecordingStreamBuilder::new("pointcloud").spawn()?;
-    render_las_in_rerun(rec, "points", "../common/data/pointclouds/2555_1137.las")?;
+    // let rec = rerun::RecordingStreamBuilder::new("switzerland").spawn()?;
+    // render_pointcloud_in_rerun(rec, "points", pointcloud_downsampled, None)?;
 
     Ok(())
 }
