77947290530b191b9f85bc9bede7b3219d1ccbb1
Daniel Pino <danielpino1121@gmail.com>
2025-03-31 21:01:29 -0400

    Test rerun with a sequence (#26)
    * Experimenting with rerun using sequences
    
    * Just render a sequence

--- fineleg/src/main.rs
+++ fineleg/src/main.rs
@@ -3,20 +3,29 @@ use fineleg::pointcloud_utils::{center_pointcloud, load_las_to_df, voxel_downsam
 use fineleg::viz::{render_pointcloud_in_plotly, render_pointcloud_in_rerun};
 
 fn main() -> Result<()> {
+    // Initialize rerun
+    let rec = rerun::RecordingStreamBuilder::new("switzerland").spawn()?;
+
     // This pointcloud has about 21M points, yikes
     let pointcloud = load_las_to_df("../common/data/pointclouds/2555_1137.las")?;
     println!("Full pointcloud: {:?}", pointcloud);
 
     // Let's downsample so my machine survives rendering
-    // let pointcloud_downsampled = pointcloud.sample_n_literal(350_000, false, false, None)?;
-    let pointcloud_downsampled = voxel_downsample(pointcloud, 1_000_000)?;
-    println!("Downsampled pointcloud: {:?}", pointcloud_downsampled);
+    // let pointcloud_downsampled = voxel_downsample(pointcloud.clone(), 1_000_000)?;
+    // let pointcloud_downsampled_2 = voxel_downsample(pointcloud, 500_000)?;
+    // println!("Downsampled pointcloud: {:?}", pointcloud_downsampled);
+
+
+    for _ in 0..10 {
+        let pointcloud_downsampled = pointcloud.sample_n_literal(50_000, false, false, None)?;
+        render_pointcloud_in_rerun(rec.clone(), "points", pointcloud_downsampled, None)?;
+    }
 
     // let centered = center_pointcloud(pointcloud_downsampled)?;
     // render_pointcloud_in_plotly(pointcloud_downsampled.clone())?;
 
-    let rec = rerun::RecordingStreamBuilder::new("switzerland").spawn()?;
-    render_pointcloud_in_rerun(rec, "points", pointcloud_downsampled, None)?;
+
+    // render_pointcloud_in_rerun(rec, "points", pointcloud_downsampled_2, None)?;
 
     Ok(())
 }
